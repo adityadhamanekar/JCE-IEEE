8f01837e46590ed20c4d377f313ce11a29f6f953
adityadhamanekar <adityadhamanekar725@gmail.com>
2024-05-13 19:44:45 +0530

    final

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -21,74 +21,76 @@ function ContactForm() {
   };
 
   return (
-    <div className='contact'>
-      <h1 className='contacth'>Connect with us</h1>
-      <form onSubmit={handleSubmit} className='needs-validation' noValidate>
-        <div className='mb-3'>
-          <label htmlFor='name' className='form-label'>
-            Name
-          </label>
-          <input
-            type='text'
-            name='name'
-            placeholder='Enter Your Name'
-            className='form-control'
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <div className='invalid-feedback'>Please Enter Your Name.</div>
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='email' className='form-label'>
-            Email
-          </label>
-          <input
-            type='email'
-            name='email'
-            placeholder='Enter your Email'
-            className='form-control'
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <div className='invalid-feedback'>Please Enter your Email.</div>
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='phone' className='form-label'>
-            Phone No.
-          </label>
-          <input
-            type='text'
-            name='phone'
-            placeholder='Enter Phone No.'
-            className='form-control'
-            value={formData.phone}
-            onChange={handleChange}
-            required
-          />
-          <div className='invalid-feedback'>Please Enter your Phone no.</div>
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='message' className='form-label'>
-            How Can we help you?
-          </label>
-          <textarea
-            name='message'
-            cols='30'
-            rows='4'
-            className='form-control'
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-          <div className='invalid-feedback'>Please fill out this field.</div>
-        </div>
-        <button type='submit' className='btn btn-dark send-btn'>
-          Send Message
-        </button>
-        <br />
-      </form>
+    <div className="contact_body">
+      <div className='contact'>
+        <h1 className='contacth'>Connect with us</h1>
+        <form onSubmit={handleSubmit} className='needs-validation' noValidate>
+          <div className='mb-3'>
+            <label htmlFor='name' className='form-label'>
+              Name
+            </label>
+            <input
+              type='text'
+              name='name'
+              placeholder='Enter Your Name'
+              className='form-control'
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <div className='invalid-feedback'>Please Enter Your Name.</div>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='email' className='form-label'>
+              Email
+            </label>
+            <input
+              type='email'
+              name='email'
+              placeholder='Enter your Email'
+              className='form-control'
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <div className='invalid-feedback'>Please Enter your Email.</div>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='phone' className='form-label'>
+              Phone No.
+            </label>
+            <input
+              type='text'
+              name='phone'
+              placeholder='Enter Phone No.'
+              className='form-control'
+              value={formData.phone}
+              onChange={handleChange}
+              required
+            />
+            <div className='invalid-feedback'>Please Enter your Phone no.</div>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='message' className='form-label'>
+              How Can we help you?
+            </label>
+            <textarea
+              name='message'
+              cols='30'
+              rows='4'
+              className='form-control'
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+            <div className='invalid-feedback'>Please fill out this field.</div>
+          </div>
+          <button type='submit' className='btn btn-dark send-btn'>
+            Send Message
+          </button>
+          <br />
+        </form>
+      </div>
     </div>
   );
 }
